3386864f51e5ef6ccfca2005c9771d334a48a0b5
zhanghuici <2474117415@qq.com>
2015-10-29 20:37:33 +0800

    update NavigationFile

---
+++ UIaviGationController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIaviGationController
+//
+//  Created by qingyun on 15/9/8.
+//  Copyright (c) 2015年 king. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIaviGationController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIaviGationController
+//
+//  Created by qingyun on 15/9/8.
+//  Copyright (c) 2015年 king. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
